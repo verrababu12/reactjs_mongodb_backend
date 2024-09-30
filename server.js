b9a7f959d43fb73b399dd93b741b6406cf732d75
veeru <veeru@gmail.com>
2024-09-30 15:01:21 +0530

    fileschanged

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const dotenv = require("dotenv");
 const UsersModel = require("./module/Users");
 dotenv.config();
